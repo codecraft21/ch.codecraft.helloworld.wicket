9f30c64c28a4005fad948c6016d5c54ab6861157
Chris <chris@codecraft.ch>
2012-02-25 22:21:46 +0100

    OPEN - task 5: deploy on GAE https://github.com/codecraft21/ch.codecraft.helloworld.wicket/issues/issue/5

--- helloworld.wicket/src/main/scala/ch/codecraft/hellowicket/WicketApplication.scala
+++ helloworld.wicket/src/main/scala/ch/codecraft/hellowicket/WicketApplication.scala
@@ -4,9 +4,17 @@
 package ch.codecraft.hellowicket
 
 import org.apache.wicket.protocol.http.WebApplication
+import org.apache.wicket.session.ISessionStore
+import org.apache.wicket.session.HttpSessionStore
 
 class WicketApplication extends WebApplication {
 
     override def getHomePage = classOf[HomePage]
+
+    //TODO not needed for config type 'deployment' 
+    override def init: Unit = {
+        super.init
+        getResourceSettings.setResourcePollFrequency(null)
+    }
 
 }
